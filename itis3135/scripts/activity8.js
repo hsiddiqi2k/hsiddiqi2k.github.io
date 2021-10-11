cd34e66ae8df8414c80b954f7c348dcdf7122c59
hsiddiqi2k <hsiddiqi@uncc.edu>
2021-10-10 23:02:34 -0400

    fgh

---
+++ itis3135/scripts/activity8.js
@@ -0,0 +1,50 @@
+// At the start of the JavaScript file, declare two empty arrays: one for person and one for salaries. 
+
+// Create three functions (in your script) and associated buttons (in your page) named addSalary(), displayResults(), and displaySalary() and attach them to the click events of the buttons. 
+
+alert("running");
+
+var salaries = [];
+var people = [];
+
+var average = 0;
+var total = 0;
+var maxSalary = 0;
+
+function addSalary()
+{
+    var inputSalary = document.getElementById("salaryNum").value;
+    var inputName = document.getElementById("employee").value;
+
+
+    salaries.push(inputSalary);
+    people.push(inputName);
+
+}
+
+function displayResults()
+{
+
+    total = 0;
+    for(var i = 0; i < salaries.length; i++) 
+    {
+        total += salaries[i];
+    }
+
+    average = total / salaries.length;
+
+    
+    
+
+    
+
+    document.getElementById("results").innerHTML = "Average value:" + average + " Highest value: " + maxSalary + " Total: " + total;
+
+}
+
+function displaySalary()
+{
+
+}
+
+
